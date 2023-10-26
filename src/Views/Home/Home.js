754b622396a66e34f2e49bdd0a33beceea5501b8
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-10-26 20:44:49 -0300

    carrousel

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import style from './Home.module.css'
 import Header from '../../components/Header/Header.jsx';
+import Carrousel from "../../components/Carrousel/Carrousel";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <div className={style.container}>
-                Aqui debe ir el modulo Home
+                <Carrousel />
             </div>
         </div>
     )
