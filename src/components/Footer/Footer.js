f1810a536d60c8edc8f154657bc22a32f6f2870a
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-11-14 07:34:03 -0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,12 @@ const Footer = () => {
     return (
         <div className={style.footer}>
             <p className={`${style.footerp} ${style.centeredContent}`}>
-                <SocialIcon url="https://www.instagram.com/federicorizzo.ok/" />
-                <SocialIcon url="https://www.facebook.com/federicorizzo.ok" />
-                &nbsp;&nbsp;mail@mail.com&nbsp;&nbsp;
-                <PhoneFilled />  351 3 415987
+                <SocialIcon url="https://www.instagram.com/federicorizzo.ok/" className={style.icon1}/>
+                <SocialIcon url="https://www.facebook.com/federicorizzo.ok" className={style.icon2}/>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                mail@mail.com
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <div className={style.phoneNumber}><PhoneFilled />  351 3 415987</div>
             </p>
         </div>
     );
