86a22e8a5df940140c74b6500249efd257a1b28c
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-11-21 21:57:43 -0300

    contact component

---
+++ src/Views/Contacto/Contacto.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import style from './Contacto.module.css'
+
+const Contacto = () => {
+    return (                 
+        <div className={style.container}>
+           <h1 className={style.contactTitle}>Contactanos por estos medios:</h1>
+           <div className={style.contactNode}>
+            <img src='https://static.wixstatic.com/media/400291_00335cc27ed84991a820056ff246bd72~mv2.gif' className={style.contactImg}/>
+            <p className={style.contactP1}>Por email a la direccion:</p>
+            <p className={style.contactP2}>admfederizo@gmail.com</p>
+           </div>
+           <div className={style.contactNode}>
+            <img src='https://static.wixstatic.com/media/400291_0924cd0a4b8c4c6baadcd82c67027bad~mv2.gif' className={style.contactImg}/>
+            <p className={style.contactP1}>Chatea con nosotros por whatsApp al:</p>
+            <p className={style.contactP2}>351 3415987</p>
+           </div>
+           <div className={style.contactNode}>
+            <img src='https://static.wixstatic.com/media/400291_9d7e9b87406e4892b4404a17fd060a1f~mv2.gif' className={style.contactImg}/>
+            <p className={style.contactP1}>Comunicate por telefono al:</p>
+            <p className={style.contactP2}>351 3415987</p>
+            <p className={style.contactP3}>Lunes a Viernes de 9:00 a 18:00 <br/>Sabados de 9:00 a 13:00</p>            
+           </div>
+           <div className={style.contactNode}>
+            <img src='https://static.wixstatic.com/media/400291_1d833b5d26ba46efb3c1c3c0eb718a09~mv2.gif' className={style.contactImg}/>
+            <p className={style.contactP1}>Visitanos en nuestras oficinas:</p>
+            <p className={style.contactP2}>Maipú 151, Oficina 2<br/>Córdoba Capital</p>
+           </div>
+        </div>        
+    )
+}
+
+export default Contacto;
--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -3,13 +3,13 @@ import style from './Home.module.css'
 import Header from '../../components/Header/Header.jsx';
 import Carrousel from "../../components/Carrousel/Carrousel";
 import celphone from '../../Assets/celphone.png';
-import Contacto from '../../Assets/Contacto.png';
 import HomeBoceto from '../../Assets/HomeBoceto.png';
 import Mapa from '../../Assets/Mapa.png';
 import MisionVisionValor from "../../components/MisionVisionValor/MisionVisionValor";
 import oportunidades from '../../Assets/oportunidades.png';
 import WhatsApp from "../../components/Whatsapp/Whatsapp";
 import Footer from "../../components/Footer/Footer";
+import Contacto from "../Contacto/Contacto";
 
 const Home = () => {
     return (
@@ -22,10 +22,10 @@ const Home = () => {
                         <img src={HomeBoceto} className={style.homeImg} />
                     </div>
                     <div className={style.home}>
-                        <MisionVisionValor />
+                        <MisionVisionValor />                        
                     </div>
-                    <div className={style.home}>
-                        <img src={Contacto} className={style.homeImg} />
+                    <div className={style.home}>                        
+                        <Contacto />
                     </div>
                     <div className={style.home}>
                         <img src={oportunidades} className={style.homeImg} />
