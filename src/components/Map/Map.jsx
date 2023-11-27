b3726faa44fa028a998e49d856b9a9658686ee9f
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-11-26 23:46:43 -0300

    map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,21 +5,36 @@ import style from './Map.module.css';
 const Map = () => {
   useEffect(() => {
     const loader = new Loader({
-      apiKey: 'AIzaSyDBz5bp3kALxN3ImD6iktXjZFR89oexaB4', 
+      apiKey: 'AIzaSyDBz5bp3kALxN3ImD6iktXjZFR89oexaB4', // Reemplaza con tu clave de API de Google Maps
       version: 'weekly',
     });
+
+    let coordenadas = { lat: -31.4155550, lng: -64.1795055 }
 
     loader.load().then(() => {
       const mapDiv = document.getElementById('map');
       const map = new window.google.maps.Map(mapDiv, {
-        center: { lat: -31.4155148, lng: -64.1821355 },
-        zoom: 18,
+        center: coordenadas,
+        zoom: 17,
+      });
+
+      // Crear un marcador rojo en las coordenadas dadas
+      const marker = new window.google.maps.Marker({
+        position: coordenadas,
+        map: map,
+        title: 'Ubicación',
+        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', // Icono rojo
       });
     });
   }, []);
 
   return (
-    <div id="map" className={style.MapContainer} />
+    <div className={style.mapDiv}>
+        <div id="map" className={style.MapContainer} />
+        <div className={style.mapTextDiv}>
+            <h2 className={style.mapH2}>Horarios de atención</h2>
+        </div>        
+    </div>
   ) 
   
 };
