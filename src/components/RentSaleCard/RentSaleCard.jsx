7465303b859775ab846920f2501b64b3b5ee7627
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-11-29 15:56:35 -0300

    Rent Sale Rent Cards

--- src/components/RentSaleCard/RentSaleCard.jsx
+++ src/components/RentSaleCard/RentSaleCard.jsx
@@ -1,26 +1,85 @@
 import React, { useRef, useState, useEffect } from 'react';
 import style from './RentSaleCard.module.css'
 import { Link } from 'react-router-dom';
-import RentSaleDb from '../RentSaleDb';
+import {TempRentDb, SalesDb, RentDb}  from '../RentSaleDb';
 
 const RentSaleCard = (props)=>{
-    const TempRentDb = RentSaleDb
+    const TRDb = TempRentDb
+    const SDb = SalesDb
+    const RDb = RentDb
 
     const imgRef = useRef();
     const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
+    // useEffect(() => {
+    //     const listNode = imgRef.current;
+    //     const imageNode = listNode.querySelectorAll('li > img')[currentImgIndex];
+
+    //     if (imageNode) {
+    //       imageNode.scrollIntoView({
+    //         behavior: 'smooth',
+    //       });
+    //     }      
+    //   }, [currentImgIndex]);  
+    
+    //   // Función para avanzar automáticamente las imágenes cada 5 segundos
+    //   useEffect(() => {
+    //     const intervalId = setInterval(() => {
+    //       // Calcular el próximo índice de imagen
+    //       const nextIndex = (currentImgIndex + 1) % TRDb.length;
+    //       setCurrentImgIndex(nextIndex)
+    //     }, 5000); // Intervalo de 5000 milisegundos (5 segundos)    
+    //     return () => {
+    //       // Limpiar el intervalo cuando el componente se desmonte
+    //       clearInterval(intervalId);
+    //     };
+    //   }, [currentImgIndex]);
 
     return(
         <div className={style.RentSaleCardContainer}>
             {/* <Link to="/Temporario"> */}
             <div className={style.tempRentCard}>
                 <h1 className={style.leftTextCard}>Alquiler Temporal</h1>
+                <div className={style.sliderContainer}>
+                    <div className={style.imagesContainer}>
+                        {/* <ul ref={imgRef}> */}
+                            {TRDb.map((image) => (
+                            //<li key={image.id}>
+                                <img src={image.image} className={style.imageCss} alt={image.alt} />
+                            //</li>
+                            ))}
+                        {/* </ul> */}
+                    </div>
+                </div>
             </div>
             {/* </Link> */}
             <div className={style.saleCard}>
                 <h1 className={style.centerTextCard}>Venta</h1>
+                <div className={style.slider2Container}>
+                    <div className={style.images2Container}>
+                        {/* <ul ref={imgRef}> */}
+                            {SDb.map((image2) => (
+                            // <li key={image2.id}>
+                                <img src={image2.image} className={style.SaleimageCss} alt={image2.alt} />
+                            //</li> 
+                            ))}
+                        {/* </ul> */}
+                    </div>
+                </div>
             </div>       
             <div className={style.rentCard}>
                 <h1 className={style.rightTextCard}>Alquiler</h1>
+                <div className={style.slider3Container}>
+                    <div className={style.images3Container}>
+                        {/* <ul ref={imgRef}> */}
+                            {RDb.map((image3) => (
+                            // <li key={image3.id}>
+                                <img src={image3.image} className={style.RentImageCss} alt={image3.alt} />
+                            // </li>
+                            ))}
+                        {/* </ul>    */}
+                    </div>
+                </div>
             </div>     
         </div>
 )}
--- src/components/RentSaleDb.js
+++ src/components/RentSaleDb.js
@@ -1,63 +1,67 @@
-const RentSaleDb = [{
+const TempRentDb = [{
     id: 1,
     alt: 'casa de verano',
     image: 'https://static.wixstatic.com/media/400291_d7d4000c212b472794fe382864073c74~mv2.jpg',
-    tex: 'VERANO'
+    text: 'VERANO'
 },{
     id: 2,
     alt: 'casa de otoño',
     image: 'https://static.wixstatic.com/media/400291_de8f8e14a0694a66b8a12955168f51a6~mv2.jpg',
-    tex: 'OTOÑO'
+    text: 'OTOÑO'
 },{
     id: 3,
     alt: 'casa de invierno',
     image: 'https://static.wixstatic.com/media/400291_1eb9adcfde06409ba5eeb19706799478~mv2.jpg',
-    tex: 'INVIERNO'
+    text: 'INVIERNO'
 },{
     id: 4,
     alt: 'casa de primavera',
     image: 'https://static.wixstatic.com/media/400291_0056ad813caf424a840220065e602445~mv2.jpg',
-    tex: 'PRIMAVERA'
-},{
-    id: 5,
-    alt: 'casa en venta',
-    image: 'https://static.wixstatic.com/media/400291_04d44e86ce63464d8da94981cb742531~mv2.jpg',
-    tex: 'CASAS'
-},{
-    id: 6,
-    alt: 'terreno en venta',
-    image: 'https://static.wixstatic.com/media/400291_568a1bae7a024e539e68ef6c95bfac41~mv2.jpg',
-    tex: 'TERRENOS'
-},{
-    id: 7,
-    alt: 'departamento en venta',
-    image: 'https://static.wixstatic.com/media/400291_0b463829e41c49188626e2d0c1a8c586~mv2.jpg',
-    tex: 'DEPARTAMENTOS'
-},{
-    id: 8,
-    alt: 'casa en barrio exclusivo',
-    image: 'https://static.wixstatic.com/media/400291_28d3ea2146884c3ba3c33ef9ec0c4832~mv2.jpg',
-    tex: 'EXCLUSIVAS'
-},{
-    id: 9,
-    alt: 'Monoambiente en alquiler',
-    image: 'https://static.wixstatic.com/media/400291_84d392b976294789be75f26c11a60790~mv2.jpg',
-    tex: 'MONOAMBIENTE'
-},{
-    id: 10,
-    alt: 'casa en alquiler',
-    image: 'https://static.wixstatic.com/media/400291_6d9748d050b24dcc8f552089cd4ccd32~mv2.jpg',
-    tex: 'CASAS'
-},{
-    id: 11,
-    alt: 'epacios de coworking',
-    image: 'https://static.wixstatic.com/media/400291_de706a0b6de64076b84e0c7c1e31bed9~mv2.jpg',
-    tex: 'COWORKING'
-},{
-    id: 12,
-    alt: 'Departamentos en alquiler',
-    image: 'https://static.wixstatic.com/media/400291_a3a061362da3475eb4ba645bba29c571~mv2.jpg',
-    tex: 'DEPARTAMENTOS'
+    text: 'PRIMAVERA'
 }]
 
-export default RentSaleDb
+const SalesDb = [{
+        id: 5,
+        alt: 'casa en venta',
+        image: 'https://static.wixstatic.com/media/400291_04d44e86ce63464d8da94981cb742531~mv2.jpg',
+        text: 'CASAS'
+    },{
+        id: 6,
+        alt: 'terreno en venta',
+        image: 'https://static.wixstatic.com/media/400291_568a1bae7a024e539e68ef6c95bfac41~mv2.jpg',
+        text: 'TERRENOS'
+    },{
+        id: 7,
+        alt: 'departamento en venta',
+        image: 'https://static.wixstatic.com/media/400291_0b463829e41c49188626e2d0c1a8c586~mv2.jpg',
+        text: 'DEPARTAMENTOS'
+    },{
+        id: 8,
+        alt: 'casa en barrio exclusivo',
+        image: 'https://static.wixstatic.com/media/400291_28d3ea2146884c3ba3c33ef9ec0c4832~mv2.jpg',
+        text: 'EXCLUSIVAS'    
+}]
+
+const RentDb =[{
+        id: 9,
+        alt: 'Monoambiente en alquiler',
+        image: 'https://static.wixstatic.com/media/400291_84d392b976294789be75f26c11a60790~mv2.jpg',
+        text: 'MONOAMBIENTE'
+    },{
+        id: 10,
+        alt: 'casa en alquiler',
+        image: 'https://static.wixstatic.com/media/400291_6d9748d050b24dcc8f552089cd4ccd32~mv2.jpg',
+        text: 'CASAS'
+    },{
+        id: 11,
+        alt: 'epacios de coworking',
+        image: 'https://static.wixstatic.com/media/400291_de706a0b6de64076b84e0c7c1e31bed9~mv2.jpg',
+        text: 'COWORKING'
+    },{
+        id: 12,
+        alt: 'Departamentos en alquiler',
+        image: 'https://static.wixstatic.com/media/400291_a3a061362da3475eb4ba645bba29c571~mv2.jpg',
+        text: 'DEPARTAMENTOS'
+}]
+
+export {TempRentDb, SalesDb, RentDb} 
