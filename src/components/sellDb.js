ac0c4f233be85ea5786bd121bae9cac81caaf91d
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-12-01 07:59:51 -0300

    database config

---
+++ src/components/sellDb.js
@@ -0,0 +1,21 @@
+const SellDd =[{
+    id: 1,    
+    titles: ["ALTA CORDOBA.", "EDIFICIO CALATHEAS.", "VENTA DPTO"],
+    temporary: false,
+    temporary_price: 0,
+    temporaty_coin: 'ARS',
+    high_season_percentaje: 10,
+    sell: true,
+    sell_price: 43500,
+    sell_coin: 'USD',
+    rent: false,
+    rent_price: 0,
+    rent_coin: 'ARS',
+    opportunities: true,
+    age: 'A estrenar',
+    rooms: 2,
+    bedrooms: 1,
+    bathrooms: 1,
+    garage: 'No disponible'
+
+}]
